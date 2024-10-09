e29e0b10b0ee1a527d75c539a8dad7f13121026f
ABgitme <abebekezhabesha@gmail.com>
2024-10-09 20:14:10 +0100

    task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,8 +51,40 @@ interface DirectorInterface {
     }
   }
   
-  // Test the function with different inputs
-  console.log(createEmployee(200));  // Expected output: Teacher instance
-  console.log(createEmployee(1000)); // Expected output: Director instance
-  console.log(createEmployee('$500')); // Expected output: Director instance
+  // Type predicate function to check if the employee is a Director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function to execute work based on the employee type
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// String literal type allowing only 'Math' or 'History' values
+type Subjects = 'Math' | 'History';
+
+// Function that takes todayClass as an argument and returns a string
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+// Test the function with different inputs
+console.log(createEmployee(200));  // Expected output: Teacher instance
+console.log(createEmployee(1000)); // Expected output: Director instance
+console.log(createEmployee('$500')); // Expected output: Director instance
   
+  // Test cases
+executeWork(createEmployee(200));  // Expected output: Getting to work
+executeWork(createEmployee(1000)); // Expected output: Getting to director tasks
+
+// Test cases
+console.log(teachClass('Math'));    // Expected output: Teaching Math
+console.log(teachClass('History')); // Expected output: Teaching History
