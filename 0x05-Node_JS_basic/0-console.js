5c357c22f3f78436480373d82885355627b7ec35
ABgitme <abebekezhabesha@gmail.com>
2024-11-26 05:38:06 +0300

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/**
+ * Prints a string to STDOUT.
+ * @param {string} message - The message to print.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+module.exports = displayMessage;
