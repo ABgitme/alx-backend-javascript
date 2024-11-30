6db59371bd71c296ee78795f9ed680ed11ae069d
ABgitme <abebekezhabesha@gmail.com>
2024-11-30 05:41:54 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
 // Display welcome message and ask for the user's name
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input from the standard input (stdin)
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Convert input to a string and remove extra spaces
-  console.log(`Your name is: ${name}`); // Display the user's name
-
-  // Close the program
-  console.log('This important software is now closing');
-  process.exit(); // Exit the process
+  const name = data.toString().trim(); // Remove any extra whitespace or newline
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
