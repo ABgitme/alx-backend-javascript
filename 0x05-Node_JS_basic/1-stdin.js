044499f085245185c3b8936d036df5bf25c115b2
ABgitme <abebekezhabesha@gmail.com>
2024-11-30 05:49:35 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,12 +3,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim(); // Remove any extra whitespace or newline
+    const name = data.toString(); // Remove any extra whitespace or newline
     process.stdout.write(`Your name is: ${name}\n`);
   });
 } else {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim(); // Remove any extra whitespace or newline
+    const name = data.toString(); // Remove any extra whitespace or newline
     process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
