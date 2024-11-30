b447a62d3a8dd4f2133d5aa97724e5a3bcbd89f2
ABgitme <abebekezhabesha@gmail.com>
2024-11-30 05:47:05 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,18 @@
 // Display welcome message and ask for the user's name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Remove any extra whitespace or newline
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim(); // Remove any extra whitespace or newline
+    process.stdout.write(`Your name is: ${name}\n`);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim(); // Remove any extra whitespace or newline
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
